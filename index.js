ce1d03e1cfec8167ff8665be323dc513ac1cdfa0
Ian Hollander <ianholla@gmail.com>
2019-06-24 11:39:32 -0400

    with blog code

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
 // https://jasonformat.com/wtf-is-jsx/
+class NoReact {
+  // transpiled JSX => virtual DOM
+  // function name must match transpiled JSX (check .babelrc)
+  static createElement(nodeName, attributes, ...args) {
+    const children = args.length ? [...args] : null;
+    return { nodeName, attributes, children };
+  }
 
-let App = (
-  <div id="foo">
-    <span>Hello</span>!<span>Hello</span>
-    <span>Hello</span>
-  </div>
-);
+  // virtual DOM => real DOM
+  static render(vnode) {
+    // Strings just convert to #text Nodes:
+    if (typeof vnode === "string") return document.createTextNode(vnode);
 
-// transpiled JSX => virtual DOM
-// function name must match transpiled JSX (check .babelrc)
-// h == React.createElement
-function h(nodeName, attributes, ...args) {
-  let children = args.length ? [].concat(...args) : null;
-  return { nodeName, attributes, children };
-}
+    // create a DOM element with the nodeName of our VDOM element:
+    const node = document.createElement(vnode.nodeName);
 
-// virtual DOM => real DOM
-function render(vnode) {
-  // Strings just convert to #text Nodes:
-  if (vnode.split) return document.createTextNode(vnode);
+    // copy attributes onto the new node:
+    const attributes = vnode.attributes || {};
+    Object.keys(attributes).forEach(key =>
+      node.setAttribute(key, attributes[key])
+    );
 
-  // create a DOM element with the nodeName of our VDOM element:
-  let n = document.createElement(vnode.nodeName);
+    // render (build) and then append child nodes:
+    if (vnode.children.length) {
+      // recursively call render on all child nodes
+      vnode.children.forEach(child => node.appendChild(NoReact.render(child)));
+    }
 
-  // copy attributes onto the new node:
-  let a = vnode.attributes || {};
-  Object.keys(a).forEach(k => n.setAttribute(k, a[k]));
-
-  // render (build) and then append child nodes:
-  (vnode.children || []).forEach(c => n.appendChild(render(c)));
-
-  return n;
+    return node;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  let dom = render(App);
+  const ITEMS = "hello there people".split(" ");
+
+  // turn an Array into list items:
+  let list = items => items.map(p => <li> {p} </li>);
+
+  // view with a call out ("partial") to generate a list from an Array:
+  let vdom = (
+    <div id="foo">
+      <p>Look, a simple JSX DOM renderer!</p>
+      <ul>{list(ITEMS)}</ul>
+    </div>
+  );
+
+  // render() converts our "virtual DOM" (see below) to a real DOM tree:
+  let dom = NoReact.render(vdom);
+
+  // Remember that "virtual DOM"? It's just JSON - each "VNode" is an object with 3 properties.
+  console.log(vdom);
+
+  // The whole process (JSX -> VDOM -> DOM) in one step:
   document.querySelector("#root").appendChild(dom);
 });
-
-console.log(foo);
